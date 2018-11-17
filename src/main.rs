4d361e4624da1344355b1cff33ee97b7d7b7edb4
Kevin Martins <kevinmmartins@gmail.com>
2018-11-17 18:58:30 -0200

    Add basic module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let hi =say_hi();
+    println!("{}", hi);
+    informations::basic();
+    informations::author();
+}
+
+fn say_hi() -> String {
+    "Hi ! The project is working".to_string()
+}
+
+#[test]
+fn test_greet() {
+    assert_eq!("Hi ! The project is working", say_hi())
+}
+
+
+mod informations {
+  pub fn basic() {
+    println!("Lets see some basic informations");
+  }
+  pub fn author() {
+    print!("Kevin ");
+    private_last_name()
+  }
+
+  fn private_last_name() {
+    print!("Martins");
+  }
 }
